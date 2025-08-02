84391bf2a1ad83c8427a4bb12bf32e3fcaa8fcfd
AweshAlam <aweshalam53@gmail.com>
2025-08-02 10:24:18 +0530

    dda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { BillListComponent } from './bill-list/bill-list.component';
 import { SalesReportComponent } from './sales-report/sales-report.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo:'', pathMatch:'full'},
     { path: '', component: HomeComponent },
     { path: 'bill', component: BillComponent },
     { path: 'item', component: ItemComponent },
